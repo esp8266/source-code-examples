b1c503720fb82dfac1fd3b2096014b7e630906db
zarya <zarya@gigafreak.net>
2014-10-27 23:59:58 +0100

    renamed wifi_connect to basic_example

--- basic_example/user/user_main.c
+++ basic_example/user/user_main.c
@@ -8,12 +8,11 @@
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
-static void user_procTask(os_event_t *events);
+static void loop(os_event_t *events);
 
-
-//Do nothing function
+//Main code function
 static void ICACHE_FLASH_ATTR
-user_procTask(os_event_t *events)
+loop(os_event_t *events)
 {
     os_delay_us(10);
 }
@@ -26,13 +25,14 @@ user_init()
     char password[64] = SSID_PASSWORD;
     struct station_config stationConf;
     
+    //Set station mode
+    wifi_set_opmode( 0x1 );
+
+    //Set ap settings
     os_memcpy(&stationConf.ssid, ssid, 32);
     os_memcpy(&stationConf.password, password, 32);
-    
-
-    wifi_set_opmode( 0x1 );
     wifi_station_set_config(&stationConf);
 
     //Start os task
-    system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
+    system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 }
